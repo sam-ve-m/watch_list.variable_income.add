2cea2857125725e45b2130736c7460b2df96dddf
João Bonfim <jpa@lionx.com.br>
2022-05-27 17:29:46 -0300

    feat: Added exception UnauthorizedError

--- func/main.py
+++ func/main.py
@@ -1,5 +1,6 @@
 # Jormungandr
 from src.domain.enums.response.code import InternalCode
+from src.domain.exceptions import UnauthorizedError
 from src.domain.validator import WatchListSymbols
 from src.services.watch_list import WatchListService
 from src.domain.response.model import ResponseModel
@@ -9,7 +10,7 @@ from heimdall_client.bifrost import Heimdall
 from http import HTTPStatus
 
 # Third party
-from flask import request
+from flask import request, Flask
 from etria_logger import Gladsheim
 
 
@@ -18,10 +19,14 @@ async def save_symbols(request=request):
     x_thebes_answer = request.headers.get('x-thebes-answer')
 
     try:
-        watch_list_symbols = WatchListSymbols(**raw_params)
         jwt_content, heimdall_status = await Heimdall.decode_payload(jwt=x_thebes_answer)
+        if not jwt_content["is_payload_decoded"]:
+            raise UnauthorizedError()
+
+        watch_list_symbols = WatchListSymbols(**raw_params)
         unique_id = jwt_content["decoded_jwt"]["user"]["unique_id"]
         result = await WatchListService.register(watch_list_symbols=watch_list_symbols, unique_id=unique_id)
+
         response = ResponseModel(
             success=result,
             code=InternalCode.SUCCESS,
@@ -32,6 +37,12 @@ async def save_symbols(request=request):
     except ValueError:
         response = ResponseModel(
             success=False, code=InternalCode.INVALID_PARAMS, message="Invalid params"
+        ).build_http_response(status=HTTPStatus.BAD_REQUEST)
+        return response
+
+    except UnauthorizedError:
+        response = ResponseModel(
+            success=False, code=InternalCode.JWT_INVALID, message="JWT invalid or not supplied."
         ).build_http_response(status=HTTPStatus.BAD_REQUEST)
         return response
 
