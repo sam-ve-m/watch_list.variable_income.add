493e9dd832a0075966f098aeb308bb6ea30a97b9
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-13 09:59:32 -0300

    feat: added variable_income in route

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
 fission env create --spec --name wtc-list-add-env --image nexus.sigame.com.br/fission-wacth-list-add:0.1.0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
 fission fn create --spec --name wtc-list-add-fn --env wtc-list-add-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
-fission route create --spec --name wtc-list-add-rt --method POST --url /watch_list/add --function wtc-list-add-fn
+fission route create --spec --name wtc-list-add-rt --method POST --url /watch_list/variable_income/add --function wtc-list-add-fn
