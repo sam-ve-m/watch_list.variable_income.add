e5d8b5f317d49338519415be70ad6a3b8656f99c
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-26 11:08:25 -0300

    "build: changed fission strategy to poolmg"

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name wtc-list-add-env --image nexus.sigame.com.br/fission-wacth-list-add:0.2.0-0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
-fission fn create --spec --name wtc-list-add-fn --env wtc-list-add-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
-fission route create --spec --name wtc-list-add-rt --method POST --url /watch_list/variable_income/add --function wtc-list-add-fn
+fission env create --spec --name wtc-list-var-add-env --image nexus.sigame.com.br/fission-wacth-list-variable-income-add:0.2.0-0 --poolsize 2 --graceperiod 3 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name wtc-list-var-add-fn --env wtc-list-var-add-env --code fission.py --executortype poolmgr --requestsperpod 10000 --spec
+fission route create --spec --name wtc-list-var-add-rt --method POST --url /watch_list/variable_income/add --function wtc-list-var-add-fn
